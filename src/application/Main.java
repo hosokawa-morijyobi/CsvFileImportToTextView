b88e6ed6125f0c768672bd969b7f16cb2622aa6c
user.email <hosokawa@morijyobi.ac.jp>
2018-12-12 14:42:30 +0900

    ListViewを追加実装 CSVファイルを読み込み後、TextAreaとListViewにデータを格納する。

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -10,23 +11,22 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	public static Main singleton;
 	private Stage stage;
+	private BorderPane root ;
 
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			singleton = this;
-			stage = primaryStage;
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,500,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+    public void start(Stage Stage) throws Exception{
 
-	}
+		singleton = this;
+		stage = Stage;
 
+		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+        Stage.setScene(scene);
+        Stage.show();
+
+    }
 	public static Main getInstance() {
 		return singleton;
 	}
@@ -35,7 +35,19 @@ public class Main extends Application {
 		return stage;
 	}
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		launch(args);
 	}
 }
+
+/*
+  try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+*/
--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -6,35 +6,61 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 import java.util.StringTokenizer;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 
-public class SampleController {
+public class SampleController implements Initializable {
 
 	@FXML
-	private TextArea textarea;
-	private ListView<String> listview;
-	private String display="";//テキストエリアに表示させるテキスト
-
+	private ListView<String> listView;
 	@FXML
-	protected void onButtonClick(ActionEvent e) {
+	private TextArea textArea;
 
-		textarea.setText("初期化");
-		textarea.setFont( Font.font( "MS Mincho" , 12 ) );
-		//ListView<String> listview = new ListView<>();
+	private  ObservableList<String> items ;
 
-		System.out.println("クリックされました！");
-		FileChooser fileChooser = new FileChooser();
+	private Stage stage;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    	stage = Main.getInstance().getStage();
+    	listView.getItems().add("One");
+    	listView.getItems().add("Two");
+    	listView.getItems().add("Three");
+
+    	this.textArea.setText("初期化");
+		textArea.setFont( Font.font( "MS Mincho" , 12 ) );
+
+		items = FXCollections.observableArrayList();
+		listView.setItems(items);
+		// 編集可能にする。
+		listView.setEditable(true);
+		//TextFieldTableCellクラスによって表セルをテキスト・フィールドとして実装できる
+		listView.setCellFactory(TextFieldListCell.forListView());
+    }
+
+//    //呼び出されて、listに対して追加を行うメソッド
+//    public void addListView(String Message) {
+//    	listView.getItems().add(Message);
+//    }
+
+    @FXML
+	protected void handleButtonAction(ActionEvent e) {
+    	FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open File");
 		fileChooser.getExtensionFilters().addAll(
 				   new FileChooser.ExtensionFilter("CSV", "*.csv"),
@@ -48,22 +74,22 @@ public class SampleController {
 		Stage stage = Main.getInstance().getStage();
 
 		File file = fileChooser.showOpenDialog(stage);
-//		File file = fileChooser.showOpenDialog(null);
 
-        if (file != null) {
-            String words = readCSV(file);
-            if (words != null) {
-            	System.out.println(words);
-            	textarea.setText(words);
-            }
+		if (file != null) {
+			ArrayList<String> array = readCSV(file);
 
-            ObservableList<String> array= readCSVToArray(file);
-            if (array != null) {
-            	listview.setItems( array );
-            }
-         }
+				items.clear();//ListViewのObservableListアイテムを消去しておく。
+			  String display= new String();
+			  for (String item : array){
+			        System.out.println(item);
+			        display = display + item+ "\r\n";
+			        items.add(item);//ListViewのObservableListアイテムとしてadd
+			   }
+		        textArea.setText(display);
 
-	}
+
+		}
+    }
 
     boolean checkBeforeReadfile(File file) {
         if (file.exists()) {
@@ -74,84 +100,25 @@ public class SampleController {
         return false;
     }
 
-    String readCSV(File file) {
+    ArrayList<String> readCSV(File file ) {
 
-    	String display="";//テキストエリアに表示させるテキスト
-
-        try {
+    	ArrayList<String> array=new ArrayList();//テキストエリアに表示させるテキスト
+    	try {
             if (checkBeforeReadfile(file)) {
                 FileInputStream in = new FileInputStream(file);
                 InputStreamReader sr = new InputStreamReader(in, "UTF-8");
-//                InputStreamReader sr = new InputStreamReader(in, "Shift_JIS");
                 BufferedReader br = new BufferedReader(sr);
 
                 String line;
-                boolean isHeader = true;
                 while ((line = br.readLine()) != null) {
                     StringTokenizer token = new StringTokenizer(line, ",");
 
-                    if (isHeader) {
+                    	String item;
                         while (token.hasMoreTokens()) {
-
-                            display=display+" " + token.nextToken();
+                        	item = (String)token.nextToken();
+                        	array.add(item);
                         }
-                        isHeader = false;
-                        display=display+"\r\n"+"----------------";
-                    } else {
-                        while (token.hasMoreTokens()) {
-                        	display=display+"\r\n" + token.nextToken();
-                        }
-                    }
 
-
-
-                }
-                return display;
-
-            } else {
-                System.out.println("No file exists or can't open.");
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("FileNotFoundException : " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("IOException : " + e.getMessage());
-        }
-		return display;
-
-
-
-    }
-
-
-    ObservableList<String> readCSVToArray(File file) {
-
-    	//ArrayList<String> array=new ArrayList();//テキストエリアに表示させるテキスト
-    	ObservableList<String>array = FXCollections.observableArrayList(  );
-        try {
-            if (checkBeforeReadfile(file)) {
-                FileInputStream in = new FileInputStream(file);
-                InputStreamReader sr = new InputStreamReader(in, "UTF-8");
-//                InputStreamReader sr = new InputStreamReader(in, "Shift_JIS");
-                BufferedReader br = new BufferedReader(sr);
-
-                String line;
-                boolean isHeader = true;
-                while ((line = br.readLine()) != null) {
-                    StringTokenizer token = new StringTokenizer(line, ",");
-
-
-                   if (isHeader) {
-                        while (token.hasMoreTokens()) {
-                        	array.add(token.nextToken());
-                        }
-                        isHeader = false;
-                        array.add("--------------");
-
-                    } else {
-                        while (token.hasMoreTokens()) {
-                        	array.add(token.nextToken());
-                        }
-                    }
                 }
                 return array;
 
@@ -165,9 +132,10 @@ public class SampleController {
         }
 		return array;
 
-
-
     }
 
-
 }
+
+
+
+
